91ca7306e5e41ac2013b76135e531bf640035fe1
Gaspar Luik <gasparluik@users.noreply.github.com>
2020-09-08 11:48:05 +0300

    Teine tund lõpetatud

---
+++ home.php
@@ -0,0 +1,28 @@
+<?php
+	$username = "Gaspar Luik";
+	$fulltimenow = date("d.m.Y H:i:s");
+	$hournow = date("H");
+	$partofday = "lihtsalt aeg";
+	if($hournow < 6) {
+		$paroftday = "uneaeg";
+	}//enne 6
+	if($hournow >= 8 and $hournow <=18) {
+		$partofday = "õppimise aeg";
+	}
+?>
+<!DOCTYPE html>
+<html lang="et">
+<head>
+	<meta charset="utf-8"/>
+	<title><?php echo $username; ?> programmeerib veebi</title>
+	
+</head>
+<body>
+	<h1><?php echo $username; ?></h1>
+	<p>See  veebileht on loodud õppetöö käigus ning ei sisalda mingit tõsiseltvõetavat sisu!</p>
+	<p>See konkreetne leht on loodud veebiprogrammeerimise kursusel lehel<a href="https://www.tlu.ee> Tallinna Ülikooli</a> Digitehnoloogiate instituudis.</p>
+	<p>Lehe avamise hetk: <?php echo $fulltimenow; ?>.</p>
+	<p><?php echo "Praegu on " .$partofday ."."; ?></p>
+	</body>
+	</html>
+	
